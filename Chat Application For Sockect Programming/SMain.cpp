32509daf5b463e6460cc0af997f2b83377673038
Mahmoud Basha <mbasga14@hotmail.com>
2024-01-01 20:37:59 +0300

    done most of code

--- Chat Application For Sockect Programming/SMain.cpp
+++ Chat Application For Sockect Programming/SMain.cpp
@@ -1,9 +1,37 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <Windows.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
+#define CLIENTSSIZE 10
+#define BUFFERSIZE 1024
 
+
+const string InitializeClientName = "100";
+const string ListCLients = "101";
+const string SendMess = "102";
+const string EndConnection = "103";
+
+struct Client {
+
+    int index;
+    long sockID;
+    struct sockaddr_in clientAddr;
+    int len;
+    char name[20] = "";
+    bool isConnectionClosed = false;
+};
+
+int clientCount = 0;
+
+struct Client clients[CLIENTSSIZE];
+HANDLE threads[CLIENTSSIZE];
+
+string SendMessageToReviever(string& message, Client& sender, Client& reciever);
+string ListAllClients(Client& currentClient);
+DWORD WINAPI HandleRequest(LPVOID param);
 void InitializeWSA();
 SOCKET& CreateSocket();
 void BindSocket(SOCKET& sock);
@@ -21,8 +49,26 @@ int main()
         BindSocket(sock);
         Listen(sock);
 
+        while (1) {
+
+            SOCKET newClientSocket = accept(sock, NULL, NULL); //(sockaddr*)&clients[clientCount].clientAddr, &clients[clientCount].len);
+
+            if (newClientSocket == SOCKET_ERROR)
+                throw new exception("Failed To Accept Request");
+
+            cout << "Socket Accpeted One Client" << endl;
+
+            clients[clientCount].sockID = newClientSocket;
+            clients[clientCount].index = clientCount;
+
+            DWORD threadId = 0;
+            threads[clientCount] = CreateThread(NULL, 0, HandleRequest, (LPVOID)&clients[clientCount], 0, &threadId);
+            clientCount++;
+        }
 
 
+        closesocket(sock);
+        WSACleanup();
         return 0;
     }
     catch (exception& ex)
@@ -39,6 +85,111 @@ int main()
     }
 }
 
+
+DWORD WINAPI HandleRequest(LPVOID param)
+{
+    Client clientData = *((Client*)param);
+
+    Client& client = clients[clientData.index];
+    char clientBuffer[BUFFERSIZE];
+
+    while (1) {
+
+        int readBytesCount = recv(client.sockID, clientBuffer, BUFFERSIZE, 0);
+        clientBuffer[readBytesCount] = '\0';
+        string input(clientBuffer);
+
+        string action = input.substr(0, 3);
+
+        if (action == InitializeClientName) {
+            string data = input.substr(4);
+            strcpy_s(client.name, data.data());
+
+            cout << client.name << endl;
+        }
+        else if (action == ListCLients) {
+            string res = ListAllClients(client);
+            send(client.sockID, res.data(), res.size(), 0);
+        }
+        else if (action == SendMess) {
+
+            int firstSeperatorIndex = input.find_first_of('|');
+            int lastSeperatorIndex = input.find_last_of('|');
+
+            cout << "Client Name is: " << client.name << endl;
+
+
+            string message = input.substr(lastSeperatorIndex + 1);
+
+            int clientIdStringSize = (input.size() - message.size() - 3);
+            int clientId = stoi(input.substr(firstSeperatorIndex, clientIdStringSize));
+
+            Client reciever = clients[clientId];
+            string res = SendMessageToReviever(message, client, reciever);
+
+            int sendToSocket;
+            if (reciever.isConnectionClosed || strlen(reciever.name) == 0)
+                sendToSocket = client.sockID;
+            else
+                sendToSocket = reciever.sockID;
+
+            send(sendToSocket, res.data(), res.size(), 0);
+        }
+        else if (action == EndConnection) {
+            client.isConnectionClosed = true;
+
+            for (int i = 0; i < CLIENTSSIZE; i++)
+            {
+                Client reciever = clients[i];
+                if (reciever.isConnectionClosed || reciever.index == client.index)
+                    continue;
+
+                string clientName(client.name);
+                string message = clientName + " teminated his connection";
+                send(reciever.sockID, message.data(), message.size(), 0);
+            }
+
+            closesocket(client.sockID);
+        }
+    }
+
+
+    closesocket(client.sockID);
+    return 0;
+}
+
+string ListAllClients(Client& currentClient) {
+    string result = "Clients Are: \n";
+
+    int notReadyClients = 0;
+
+    for (int i = 0; i < CLIENTSSIZE; i++) {
+        Client client = clients[i];
+
+        if (client.index == currentClient.index || client.isConnectionClosed || strlen(client.name) == 0) {
+            notReadyClients++;
+            continue;
+        }
+
+        result += (to_string(client.index) + ":" + client.name) + "\n";
+    }
+    
+    if (notReadyClients == CLIENTSSIZE)
+        result = "There isn't Available Clients \n";
+
+    return result;
+}
+
+string SendMessageToReviever(string& message,Client& sender, Client& reciever) {
+ 
+    if (reciever.isConnectionClosed || strlen(reciever.name) == 0)
+        return "This Client Isn't Avaiable Now";
+
+    string nameOfSender(sender.name);
+
+    return nameOfSender + ": " + message;
+
+}
 
 void InitializeWSA() {
     WSADATA wsaData;
@@ -80,14 +231,7 @@ void BindSocket(SOCKET& sock) {
 }
 
 void Listen(SOCKET& sock) {
-
-    sockaddr_in bindingData;
-    bindingData.sin_family = AF_INET;
-    bindingData.sin_port = htons(55555);
-
-    inet_pton(AF_INET, "127.0.0.1", &bindingData.sin_addr);
-
-    int listedResult = listen (sock, 4);
+    int listedResult = listen (sock, CLIENTSSIZE);
     if (listedResult == SOCKET_ERROR)
         throw new exception("Failed To Listen a Socket !\n");
 
--- Client/CMain.cpp
+++ Client/CMain.cpp
@@ -1,11 +1,23 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
+
+#define BUFFERSIZE 1024
+
+const string InitializeClientName = "100";
+const string ListCLients = "101";
+const string SendMess = "102";
+const string EndConnection = "103";
+
+bool isMessageValid(string input);
 void InitializeWSA();
 SOCKET& CreateSocket();
 void Connect(SOCKET& sock);
+void SendData(SOCKET& sock);
+DWORD RecieveData(LPVOID sockPr);
 
 
 
@@ -18,8 +30,17 @@ int main()
     {
         InitializeWSA();
         sock = CreateSocket();
+
+        //Sleep(4000);
         Connect(sock);
 
+        DWORD threadId = 0;
+        HANDLE t = CreateThread(NULL, 0, RecieveData, (LPVOID)&sock, 0, &threadId);
+
+        SendData(sock);
+
+        closesocket(sock);
+        WSACleanup();
         return 0;
     }
     catch (exception& ex)
@@ -74,5 +95,94 @@ void Connect(SOCKET& sock) {
         throw new exception("Failed To Connect a Socket !\n");
 
     cout << "Socket is connected" << endl;
+}
+
+void SendData(SOCKET& sock) {
+
+    string input;
+
+    cout << "Enter Your Name:";
+    getline(std::cin, input);
+
+
+    string message = InitializeClientName + '|' + input;
+    send(sock, message.data(), message.size(), 0);
+
+    cout << "If You Want To End The Program Write: exit" << endl;
+    cout << "If You Want To Refresh The Client List Write: list" << endl;
+    cout << "Please, When You Write Your Message Write First The Number Of CLient You Want To Send The Message To Then Write | Then Your Message" << endl;
+
+    Sleep(1000);
+
+
+    message = ListCLients;
+    send(sock, message.data(), message.size(), 0);
+
+
+    while (1) {
+
+        cout << "Write Your Message:";
+
+        cin >> input;
+        if (input == "exit") {
+            input = EndConnection;
+            send(sock, input.data(), input.size(), 0);
+            return;
+        }
+        else if (input == "list") {
+            input = ListCLients;
+            send(sock, input.data(), input.size(), 0);
+
+            Sleep(1000);
+            continue;
+        }
+
+        if (!isMessageValid(input)) {
+            cout << "Please, When You Write Your Message Write First The Number Of CLient You Want To Send The Message To Then Write | Then Your Message" << endl << endl;
+            continue;
+        }
+
+        input = SendMess + '|' + input;
+        send(sock, input.data(), input.size(), 0);
+    }
+}
+
+DWORD RecieveData(LPVOID sockPr) {
+
+    SOCKET sock = *((SOCKET*)sockPr);
+
+    while (1) {
+
+        char data[BUFFERSIZE];
+        int read = recv(sock, data, BUFFERSIZE, 0);
+        data[read] = '\0';
+        string input(data);
+
+        cout << input << endl;
+
+    }
+
+    return 0;
+}
+
+
+bool isMessageValid(string input) {
+    int firstSpaceIndex = input.find('|');
+
+    if (firstSpaceIndex == -1)
+        return false;
+
+    string clientStrId = input.substr(0, firstSpaceIndex);
+    string message = input.substr(0, firstSpaceIndex);
+
+    try
+    {
+        int clientId = stoi(clientStrId);
+        return true;
+    }
+    catch (const std::exception&)
+    {
+        return false;
+    }
 }
 
